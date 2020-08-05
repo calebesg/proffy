6dac6fed0207c1302c2da6cacc0c99aa1e705a7e
CalebeSG <calebeguimaraes10@gmail.com>
2020-08-05 14:28:41 -0300

    create all migrations

---
+++ server/src/database/migrations/01_create_classes.ts
@@ -0,0 +1,20 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+  return await knex.schema.createTable('classes', table => {
+    table.increments('id').primary();
+    table.string('subject').notNullable();
+    table.decimal('cost').notNullable();
+    
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('classes');
+}
