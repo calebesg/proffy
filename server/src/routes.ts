4bbf283c2542ad6a39076904b3a12212ec7e9d05
CalebeSG <calebeguimaraes10@gmail.com>
2020-08-05 16:55:04 -0300

    created server

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 
 routes.post('/classes', ClassesController.create);
 routes.get('/classes', ClassesController.index);
+
+routes.post('/connections', ConnectionsController.create);
+routes.get('/connections', ConnectionsController.index);
 
 export default routes;
