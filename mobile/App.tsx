d7380b25b0d4eb8e318e12fa2a1d010206a4f810
CalebeSG <calebeguimaraes10@gmail.com>
2020-08-06 22:15:36 -0300

    config react-navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { AppLoading } from 'expo';
 
+import AppStack from './src/routes/AppStack';
+
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
-
-import Landing from './src/pages/Landing';
 
 export default function App() {
   let [fontLoaded] = useFonts({
@@ -21,7 +21,7 @@ export default function App() {
 
   return (
     <>
-      <Landing />
+      <AppStack />
       <StatusBar style="light" />
     </>
   );
